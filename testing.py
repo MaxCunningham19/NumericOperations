3bda9ed78f99f749324ae2a9a750af2a78d5d112
MaxCunningham19 <78432096+MaxCunningham19@users.noreply.github.com>
2022-09-21 17:34:23 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,4 +1,13 @@
-from numericOperation import muliply_op
+import numericOperation
 
 def test_multi():
   assert muliply_op(10,11) == 110
+  
+def test_sub():
+  assert sub_op(11,10) == 1
+  
+def test_add():
+  assert add_op(11,10) == 21
+
+def test_pow():
+  assert pow_op(2,2) == 4
