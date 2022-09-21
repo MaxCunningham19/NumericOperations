f3a13206d979fd279e721b9b3b18fda90babbe71
MaxCunningham19 <78432096+MaxCunningham19@users.noreply.github.com>
2022-09-21 17:32:09 +0100

    Update numericOperation.py

--- numericOperation.py
+++ numericOperation.py
@@ -1,2 +1,11 @@
-def muliply_op(n1, n2):
-  return n1*n2 +2
+def multiply_op(n1, n2):
+  return n1*n2
+
+def add_op(n1,n2):
+  return n1+n2
+
+def sub_op(n1,n2):
+  return n1-n2
+
+def pow_op(n1,n2):
+  return n1**n2
